85d7b2266e11ae00ea8c3ce26610744a53a3e8b0
amit-gupta11 <ag98888@gmail.com>
2021-08-20 15:18:15 +0530

    Initial commit

--- src/main/java/com/demo/common/Test.java
+++ src/main/java/com/demo/common/Test.java
@@ -1,6 +1,15 @@
 package com.demo.common;
 
-public class Test {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Test {
+        public static void main(String [] args){
+            ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        A temp=(A) context.getBean("aReference");
+        System.out.println(temp.getX());
+        System.out.println(temp.getObj().getY());
+            System.out.println(temp);
+        }
     }
 
