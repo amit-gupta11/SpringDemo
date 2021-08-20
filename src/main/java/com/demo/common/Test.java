0c0368cf9422f0ee96fa5cec17cfc1d778885892
amit-gupta11 <ag98888@gmail.com>
2021-08-20 10:24:55 +0530

    Initial commit

--- src/main/java/com/demo/common/Test.java
+++ src/main/java/com/demo/common/Test.java
@@ -1,17 +1,6 @@
 package com.demo.common;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Test {
 
-    public class Test {
-        public static void main(String[] args) {
-            try {
-                ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-                Employee employee = (Employee) context.getBean("employee");
-                System.out.println(employee);
-            }catch(Exception exception){
-                exception.printStackTrace();
-            }
-        }
     }
 
