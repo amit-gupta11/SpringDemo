2f80a2695e1e4a6a1c84c76a7277202762bf5c50
amit-gupta11 <ag98888@gmail.com>
2021-08-20 15:57:27 +0530

    Initial commit

--- src/main/java/com/demo/common/Test.java
+++ src/main/java/com/demo/common/Test.java
@@ -1,6 +1,17 @@
 package com.demo.common;
 
-public class Test {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Test {
+    public static void main(String[] args) {
+        try {
+            ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+            Addition add = (Addition) context.getBean("add");
+            add.dosum();
+        }catch(Exception exception){
+            exception.printStackTrace();
+        }
     }
 
+}
