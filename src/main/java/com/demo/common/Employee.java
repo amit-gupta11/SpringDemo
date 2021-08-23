cc8a7d6b48eb0bd63516c34e374b28e50e5fbe81
amit-gupta11 <ag98888@gmail.com>
2021-08-23 15:53:28 +0530

    Initial commit

--- src/main/java/com/demo/common/Employee.java
+++ src/main/java/com/demo/common/Employee.java
@@ -1,5 +1,27 @@
 package com.demo.common;
 
 public class Employee {
+    private Address address;
 
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Employee() {
+    }
+
+    public Employee(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "address=" + address +
+                '}';
+    }
 }
--- src/main/java/com/demo/common/Test.java
+++ src/main/java/com/demo/common/Test.java
@@ -1,6 +1,13 @@
 package com.demo.common;
 
-public class Test {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Test {
+    public static void main(String[] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("AutoConfig.xml");
+     Employee employee1 =  context.getBean("employee1",Employee.class);
+    System.out.println(employee1);
     }
+}
 
