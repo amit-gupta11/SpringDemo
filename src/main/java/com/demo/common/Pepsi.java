ae7a63c00da3cf9878e96ed27c62dc53fa3e69ae
amit-gupta11 <ag98888@gmail.com>
2021-08-23 11:50:32 +0530

    Initial commit

---
+++ src/main/java/com/demo/common/Pepsi.java
@@ -0,0 +1,36 @@
+package com.demo.common;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Pepsi implements InitializingBean, DisposableBean {
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Pepsi() {
+    }
+
+    @Override
+    public String toString() {
+        return "Pepsi{" +
+                "price=" + price +
+                '}';
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Taking Pepsi : init");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Going to put bottle to shop : destroy");
+    }
+}
--- src/main/java/com/demo/common/Test.java
+++ src/main/java/com/demo/common/Test.java
@@ -6,9 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args){
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Samosa samosa1 =(Samosa) context.getBean("samosa1");
-        System.out.println(samosa1);
-        //
+        Pepsi pepsi1 =(Pepsi) context.getBean("pepsi1");
+        System.out.println(pepsi1);
         context.registerShutdownHook();
     }
 }
