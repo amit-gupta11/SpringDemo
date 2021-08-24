ed3463f2bd88f4853706465c6fa44450ca8c6efd
amit-gupta11 <ag98888@gmail.com>
2021-08-24 12:38:05 +0530

    Initial Commit

--- src/main/java/com/demo/common/Test.java
+++ src/main/java/com/demo/common/Test.java
@@ -1,6 +1,19 @@
 package com.demo.common;
 
-public class Test {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Test {
+    public static void main(String[] args) {
+        try {
+            ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+            Person person1 = (Person) context.getBean("person1",Person.class);
+            System.out.println(person1);
+            System.out.println(person1.getFriends().getClass().getName());
+            System.out.println(person1.getFeeStructure().getClass().getName());
+        }catch(Exception exception){
+            exception.printStackTrace();
+        }
+    }
     }
 
